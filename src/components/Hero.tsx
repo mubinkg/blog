2baab6469418a463f3904b43051a2e1898938fd0
mubin <mubin.ice.ru@gmail.com>
2024-09-16 23:39:07 +0600

    add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+import LinkedinSmall from './icons/LinkedinSmall'
+import InstaSmall from './icons/InstaSmall'
+import { FbSmall } from './icons/FbSmall'
+import YtSmall from './icons/YtSmall'
+
+const Hero = () => {
+    return (
+        <div>
+            <div className='flex justify-between'>
+                <Image src='/person1.png' width="512" height="702" alt="" />
+                <div className='flex flex-col'>
+                    <Image src='/person2.png' width="784" height="496" alt="" />
+                    <div style={{background: "rgba(33, 56, 62, 1)", marginLeft: "auto", borderRadius: "20px"}} className='w-10/12 mt-[117px] py-8 px-14' >
+                        <h2 className='text-white mb-2'>Заследи не на социјалните медиуми</h2>
+                        <div className='flex gap-4'>
+                            <LinkedinSmall/>
+                            <InstaSmall/>
+                            <FbSmall/>
+                            <YtSmall/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
