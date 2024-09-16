ac6506e6c581a666a2eeca5c8269d0a4cbeee01d
mubin <mubin.ice.ru@gmail.com>
2024-09-16 23:58:17 +0600

    update sidebar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 <Image src='/person1.png' width="512" height="702" alt="" />
                 <div className='flex flex-col'>
                     <Image src='/person2.png' width="784" height="496" alt="" />
-                    <div style={{background: "rgba(33, 56, 62, 1)", marginLeft: "auto", borderRadius: "20px"}} className='w-10/12 mt-[117px] py-8 px-14' >
+                    <div style={{background: "rgba(33, 56, 62, 1)", marginLeft: "auto"}} className='w-10/12 mt-[117px] py-8 px-14 rounded-l-full' >
                         <h2 className='text-white mb-2'>Заследи не на социјалните медиуми</h2>
                         <div className='flex gap-4'>
                             <LinkedinSmall/>
