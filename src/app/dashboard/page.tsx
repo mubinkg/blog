195a963d27006011b7c1537b29cb979028c5ae51
mubin <mubin.ice.ru@gmail.com>
2024-09-22 00:34:07 +0600

    add progress

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,18 @@
+import Sliders from "@/components/Sliders";
 import Profile from "@/components/dashboard/Profile";
+import Progress from "@/components/dashboard/Progress";
 
 export default function Page() {
     return (
-        <div className="container">
-            <div className="mt-[88px]">
+        <div>
+            <div className="mt-[88px] container">
                 <Profile />
+            </div>
+            <div className="my-40">
+                <Sliders/>
+            </div>
+            <div className="conatiner">
+                <Progress/>
             </div>
         </div>
     )
---
+++ src/components/dashboard/Progress.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Progress = () => {
+  return (
+    <div className='flex'>
+      <div></div>
+      <div className='w-8/12'></div>
+    </div>
+  )
+}
+
+export default Progress
